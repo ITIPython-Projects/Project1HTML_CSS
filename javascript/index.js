ee00542c07f79ae4b4580c363452d1c50eb9facd
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2022-12-16 07:49:26 +0200

    'WE MAKE THIS' SECTION sub of the container 'CONTENT' has been add With Buttons whis javaScript

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-//------------------------- Search Button Click -------------------------
+//------------------------- NAVEBARE: Search Button Click -------------------------
 let hasPressed = 0
 function displayInput(){
     if(hasPressed){
@@ -11,3 +11,15 @@ function displayInput(){
     document.getElementById('searchIconButton').title= 'Click Me To Close' 
     hasPressed = 1
 }
+//------------------------- CONTENT Section:'We MAke This' Display Projects Button -------------------------
+var arrButtons = document.getElementsByTagName('button')
+Array.from(arrButtons).forEach(function(element) {
+    element.addEventListener('click', function(){
+    document.getElementsByClassName('ProjectButtonActiv')[0].classList.toggle("ProjectButtonActiv");
+    this.classList.add('ProjectButtonActiv')
+    });
+  });
+// function removeActiveClass(_this){
+//     document.getElementsByClassName('ProjectButtonActiv')[0].classList.toggle("ProjectButtonActiv");
+//     _this.classList.add('ProjectButtonActiv')
+// }
