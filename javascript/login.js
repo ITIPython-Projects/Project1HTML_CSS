20300d56676da55ac6637d0616630582a75aef12
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2022-12-17 22:49:30 +0200

    Merge Login And Registration Pages on the same page and remove the old registration pag

---
+++ javascript/login.js
@@ -0,0 +1,16 @@
+//------------------------------------- Change Login To Regesteration -------------------------
+function replaceToRegistration(){
+    var email      = document.getElementById('email')
+    var repassword =document.getElementById('repassword')
+    /** Set Style*/
+    email.style.display='inline';
+    email.setAttribute('required','');
+    repassword.style.display='inline';
+    repassword.setAttribute('required','')
+    //remmember me
+    document.getElementsByClassName('remmemberMe')[0].style.display='none'
+
+    /** Set submit Button Value*/
+    document.getElementById('submit').value='Submit'
+    document.getElementsByTagName('form')[0].action='redirection.html'
+}
