0d32395111b747d6365c95f7473f5f48af789679
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2022-12-25 03:05:49 +0200

    SOLVE 'BUG' SnowMan Make it Work When Page Load Firt

--- javascript/JQindex.js
+++ javascript/JQindex.js
@@ -1,28 +1,3 @@
-// $(document).ready(function () {
-//     $(document).on('mousemove', function (e) {
-//         $('#follwoMe').css({
-//             display: "block",
-//             left: e.pageX,
-//             top: e.pageY
-//         });
-//     });
-//     var timeout;
-//     $(document).on('mousemove', function (event) {
-//         if (timeout !== undefined) {
-//             window.clearTimeout(timeout);
-//         }
-//         timeout = window.setTimeout(function () {
-//             $(event.target).trigger('mousemoveend');
-//         }, 2300);
-//     });
-//     $(document).on('mousemoveend', function (e) {
-//         $('#follwoMe').css({
-//             left: e.pageX,
-//             top: e.pageY,
-//             display: "none"
-//         });
-//     });
-// });
 window.onload = () => {
     $(document).on('mousemove', function (e) {
         $('#follwoMe').css({
