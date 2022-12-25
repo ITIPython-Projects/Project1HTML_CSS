28fa63921ec8d5df5498cd7e334634bb736f8a22
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2022-12-25 03:00:51 +0200

    SOLVE 'BUG' SnowMan Make it Work When Page Load Firt

--- javascript/JQindex.js
+++ javascript/JQindex.js
@@ -1,5 +1,29 @@
 // $(document).ready(function () {
-
+//     $(document).on('mousemove', function (e) {
+//         $('#follwoMe').css({
+//             display: "block",
+//             left: e.pageX,
+//             top: e.pageY
+//         });
+//     });
+//     var timeout;
+//     $(document).on('mousemove', function (event) {
+//         if (timeout !== undefined) {
+//             window.clearTimeout(timeout);
+//         }
+//         timeout = window.setTimeout(function () {
+//             $(event.target).trigger('mousemoveend');
+//         }, 2300);
+//     });
+//     $(document).on('mousemoveend', function (e) {
+//         $('#follwoMe').css({
+//             left: e.pageX,
+//             top: e.pageY,
+//             display: "none"
+//         });
+//     });
+// });
+window.onload = () => {
     $(document).on('mousemove', function (e) {
         $('#follwoMe').css({
             display: "block",
@@ -23,4 +47,4 @@
             display: "none"
         });
     });
-// });
+  };
